f7a3975c65085be67f36c4803ee4fe896147bd13
OyukiCV <oyukivel@hotmail.com>
2019-06-13 18:27:46 -0500

    primer actualizacion proyecto de cifrado

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const botonCod = document.getElementById("buttonCod");
+const mensaje = document.getElementById("mensaje");
+
+function mensajeIntro(mensaje) => {
+	const mensaje = string.fromCharCode(ASCII);
+	
+})
+
+
+botonCod.addEventListener("click", () => {
+	console.log(mensaje.value);
+})
